68eec5f6aece6eeac9b53c748b6324b9982a2257
Solomon <solomonghebrelibanos@gmail.com>
2024-09-20 18:05:45 -0700

    order component updated

--- models/user.js
+++ models/user.js
@@ -2,7 +2,54 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
+const orderSchema = new Schema({
+  nikeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Nike",
+    required: true,
+  },
+  shoeSize: {
+    type: String,
+    default: "",
+  },
+
+  country: {
+    type: String,
+    default: "",
+  },
+  fullName: {
+    type: String,
+    default: "",
+  },
+  phoneNum: {
+    type: String,
+    default: "",
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  state: {
+    type: String,
+    default: "",
+  },
+  zipCode: {
+    type: String,
+    default: "",
+  },
+});
+
 const userSchema = new Schema({
+  username: {
+    type: String,
+    default: "",
+    required: true,
+    unique: true,
+  },
   firstname: {
     type: String,
     default: "",
@@ -15,9 +62,7 @@ const userSchema = new Schema({
     type: Boolean,
     default: false,
   },
-  facebookId: {
-    type: String,
-  },
+  orders: [orderSchema],
 });
 
 userSchema.plugin(passportLocalMongoose);
--- routes/users.js
+++ routes/users.js
@@ -55,6 +55,7 @@ userRouter.post(
       token: token,
       id: req.user._id,
       firstName: req.user.firstname,
+      orders: req.user.orders,
       //user: req.user,
       status: "You are successfully logged in!",
     });
@@ -75,5 +76,88 @@ userRouter.get(
     });
   }
 );
+
+userRouter.get("/checkJWTtoken", (req, res) => {
+  passport.authenticate("jwt", { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    if (!user) {
+      return res.json({ status: "JWT invalid!", success: false, err: info });
+    } else {
+      return res.json({ status: "JWT valid!", success: true, user: user });
+    }
+  })(req, res);
+});
+
+userRouter
+  .route("/orders")
+  .get(authenticate.verifyUser, (req, res, next) => {
+    User.findById(req.user._id)
+      .then((user) => {
+        if (user) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(user.orders);
+        } else {
+          err = new Error(`User ${req.user._id} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post(authenticate.verifyUser, (req, res, next) => {
+    User.findById(req.user._id)
+      .then((user) => {
+        if (user) {
+          console.log("req.body", req.body);
+          user.orders.push(req.body);
+          user
+            .save()
+            .then((user) => {
+              console.log("user", user);
+              res.statusCode = 201;
+              res.setHeader("Content-Type", "application/json");
+              res.json(user.orders[user.orders.length - 1]);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`User ${req.user._id} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /user/orders`);
+  })
+  .delete(authenticate.verifyUser, (req, res, next) => {
+    User.findById(req.user._id)
+      .then((user) => {
+        if (user) {
+          user.orders = user.orders.filter(
+            (order) => order.nikeId != req.body.nikeId
+          );
+          user
+            .save()
+            .then((user) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(user.orders);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`User ${req.user._id} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
 
 module.exports = userRouter;
